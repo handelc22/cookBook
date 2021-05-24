8be6a305cfc5dc5db94951922c870ee71223a7d7
Colleen Handel <handelc22@gmail.com>
2021-05-24 12:34:17 -0400

    Initial set up

---
+++ client/components/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>hey there is this working</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
