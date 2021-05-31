a686c6ea3e7daaef0d33fdc5afdc5b2882179c47
Colleen Handel <handelc22@gmail.com>
2021-05-30 23:54:58 -0400

    Implemented functionality for calendar to show current month(s)

--- client/components/Calendar.js
+++ client/components/Calendar.js
@@ -4,21 +4,45 @@ import CalendarDay from './CalendarDay';
 
 const Calendar = ({ date, shiftWeekForward, shiftWeekBackwards }) => {
   if (date) {
+    var startingDate = date.getDate();
     var week = new Array(7).fill(0).map((space, index) => {
       var startingDate = date.getDate();
       var currentDate = new Date(date);
       currentDate.setDate(startingDate + index);
       return currentDate;
     });
+
+    var yearAtStartOfWeek = date.getFullYear();
+    var monthAtStartOfWeek = date.toString().slice(4, 7);
+    var monthAtEndOfWeek = new Date(date);
+    monthAtEndOfWeek.setDate(startingDate + 7);
+    var yearAtEndOfWeek = monthAtEndOfWeek.getFullYear();
+    monthAtEndOfWeek = monthAtEndOfWeek.toString().slice(4, 7);
+    if (monthAtStartOfWeek !== monthAtEndOfWeek) {
+      if (yearAtStartOfWeek !== yearAtEndOfWeek) {
+        var month = `${monthAtStartOfWeek} ${yearAtStartOfWeek} - ${monthAtEndOfWeek} ${yearAtEndOfWeek}`
+      } else {
+        var month = `${monthAtStartOfWeek} - ${monthAtEndOfWeek} ${yearAtStartOfWeek}`
+      }
+    } else {
+      var month = `${monthAtStartOfWeek} ${yearAtStartOfWeek}`;
+    }
   }
 
+
   return date ? (
-    <div className='calendar'>
-      <Button variant="light" onClick={shiftWeekBackwards} style={{ position: 'relative', left: '2.5%' }}>{'<'}</Button>
-      <div className="current-week" style={{ maxWidth: 'calc(90% - 64px)', display: 'inline-block', margin: 'auto' }}>
+    <div className='calendar' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '80px', position: 'relative' }}>
+
+      <div style={{ position: 'absolute', top: '10px' }}>
+        {month}
+      </div>
+
+      {/* <Card.Title className='calendar-date'>{`${date.toString().slice(4, 10)},${date.toString().slice(10, 15)}`}</Card.Title> */}
+      <Button variant="light" onClick={shiftWeekBackwards} style={{ margin: '1%' }}>{'<'}</Button>
+      <div className="current-week" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '90%' }}>
         {week.map((day, index) => <CalendarDay key={index} date={day} index={index}/>)}
       </div>
-      <Button variant="light" onClick={shiftWeekBackwards} style={{ position: 'relative', right: '2.5%' }}>{'>'}</Button>
+      <Button variant="light" onClick={shiftWeekForward} style={{ margin: '1%' }}>{'>'}</Button>
     </div>
   ) : null;
 }
--- client/components/CalendarDay.js
+++ client/components/CalendarDay.js
@@ -1,18 +1,104 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
-var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var weekday = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
 const CalendarDay = ({ date, index }) => {
   return (
-    <Card className={`day-${index}`} style={{ width: '14%', maxWidth: '200px', display: 'inline-block' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card className={`day-${index}`} style={{ display: 'inline-block', margin: '5px', width: '15%', backgroundColor: '#eee', border: 'none' }}>
       <Card.Body>
-        <Card.Title className='calendar-day'>{weekday[date.getDay()]}</Card.Title>
-        <Card.Title className='calendar-date'>{`${date.toString().slice(4, 10)},${date.toString().slice(10, 15)}`}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
+        <Card.Title className='calendar-day' style={{ backgroundColor: '#fff', color: '#000', backgroundColor: '#eee' }}>{weekday[date.getDay()]}</Card.Title>
+        <Card.Title className='calendar-date' style={{ textAlign: 'center' }}>{date.getDate()}</Card.Title>
+        <Card.Text style={{ height: '200px', overflow: 'scroll', paddingTop: '5px' }}>
+          <span className='calendar-row'>
+            <span className='time'>
+              8 AM
+            </span>
+            <span className='calendar-block first'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              9 AM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              10 AM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              11 AM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              12 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              1 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              2 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              3 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              4 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              5 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              6 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              7 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              8 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              9 PM
+            </span>
+            <span className='calendar-block'></span>
+          </span>
+          <span className='calendar-row'>
+            <span className='time'>
+              10 PM
+            </span>
+          </span>
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
